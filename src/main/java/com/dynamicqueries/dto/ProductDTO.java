4a09dc69258c3fed073adaf447505e5ecf8eb5f2
Alex Souza <alexsouza_kb@outlook.com>
2023-03-09 23:24:43 -0300

    :sparkles: feat: DTO class

---
+++ src/main/java/com/dynamicqueries/dto/ProductDTO.java
@@ -0,0 +1,25 @@
+package com.dynamicqueries.dto;
+
+import com.dynamicqueries.model.enums.Gender;
+import com.dynamicqueries.model.enums.ProductBrand;
+import com.dynamicqueries.model.enums.ProductCategory;
+import com.dynamicqueries.model.enums.ProductColor;
+import com.dynamicqueries.model.enums.ProductSize;
+import com.dynamicqueries.model.enums.ProductType;
+import lombok.Builder;
+
+import java.math.BigDecimal;
+
+@Builder
+public class ProductDTO {
+
+    private String name;
+    private String description;
+    private ProductCategory category;
+    private ProductType type;
+    private Gender gender;
+    private ProductSize size;
+    private ProductColor color;
+    private ProductBrand brand;
+    private BigDecimal price;
+}
